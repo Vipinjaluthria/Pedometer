65a636dd0d0d8a853413f261c878c39a38dcee0c
gptshubham595 <gptshubham595@gmail.com>
2021-02-25 03:10:15 +0530

    a

--- src/main/java/com/j4velin/pedometer/LeaderAdapter.java
+++ src/main/java/com/j4velin/pedometer/LeaderAdapter.java
@@ -3,6 +3,7 @@ package com.j4velin.pedometer;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,37 +23,45 @@ public class LeaderAdapter extends RecyclerView.Adapter<LeaderView> {
     public LeaderAdapter(ArrayList<LeaderBoard> leaderBoards) {
         this.leaderBoards = leaderBoards;
     }
-    ArrayList<LeaderBoard>leaderBoards;
+
+    ArrayList<LeaderBoard> leaderBoards;
 
     private Context context;
 
     @NonNull
     @Override
     public LeaderView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.leaderboardscore,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.leaderboardscore, parent, false);
         context = parent.getContext();
         return new LeaderView(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull LeaderView holder, int position) {
-        if(position==0){
+        if (position == 0) {
             holder.card.setBackgroundResource(R.drawable.leaderboard_bg);
-            ViewGroup.LayoutParams params = holder.card.getLayoutParams();;
-            params.height= 800;
+            ViewGroup.LayoutParams params = holder.card.getLayoutParams();
+            ;
+            params.height = 800;
             holder.card.setLayoutParams(params);
-            int color = Integer.parseInt("ffffff", 16)+0xFF000000;
+            int color = Integer.parseInt("ffffff", 16) + 0xFF000000;
             holder.textView.setTextColor(color);
             holder.timeview.setTextColor(color);
             holder.idview.setTextColor(color);
-            holder.imageView.setImageResource(R.drawable.test);
+            holder.imageView.setImageResource(R.drawable.round_button);
         }
-        try{Picasso.with(context).load(leaderBoards.get(position).getPhoto()).into(holder.imageView);}catch(Exception e){
+        try {
+            if (leaderBoards.get(position).getPhoto() != null) {
+                Picasso.with(context).load(Uri.parse(leaderBoards.get(position).getPhoto())).into(holder.imageView);
+            } else {
+                holder.imageView.setImageResource(R.drawable.round_button);
+            }
+        } catch (Exception e) {
             holder.imageView.setImageResource(R.drawable.round_button);
         }
         holder.textView.setText(leaderBoards.get(position).getName());
-       // holder.timeview.setText(leaderBoards.get(position).getTime());
-        holder.idview.setText(String.valueOf(position+1));
+//        holder.timeview.setText(leaderBoards.get(position).getTime());
+        holder.idview.setText(String.valueOf(position + 1));
 
     }
 
